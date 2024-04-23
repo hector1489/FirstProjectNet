5a715ab479c4e7edfea39d83eb512078b9b62301
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-04-23 19:01:10 -0400

    [fix] reset

--- obj/Debug/net8.0/FirstProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284fd3016f2cefffe645b152ff28f76c5ef1aac6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f706fee876daef6b39690d2de7dc12d8f3b63541")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
